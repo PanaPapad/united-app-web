76f83a824054ad4b60331f4e344fdaaaede5d56f
Panagiotis <panagiotis---@live.com>
2026-05-03 00:12:06 +0300

    Sample page until we have something

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
-import Button from '../components/Button/Button';
+import "./Home.scss";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Man United</h1>
-      <Button label="View History" />
-    </div>
+    <main className="home">
+      <span className="home__badge">🔴</span>
+      <h1 className="home__title">Manchester United</h1>
+      <hr className="home__divider" />
+      <p className="home__subtitle">The Theatre of Dreams</p>
+      <div className="home__coming-soon">More coming soon</div>
+    </main>
   );
 }
